2505d34ba107f8a81c01f9bf7df83b7b567498f3
Jim Hester <james.f.hester@gmail.com>
2019-01-11 13:37:25 -0500

    Convert all activity links to the separate activity repos

--- activities/22_read-source-02.R
+++
@@ -1,7 +0,0 @@
-# How does readr::read_lines work?
-
-# How does rlang::quo work?
-
-# Where is the development source for the 'Window Function' dplyr vignette?
-
-# Found a typo in ?ggplot2::geom_histogram, what code needs to change?
